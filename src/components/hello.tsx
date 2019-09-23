3500bdfe767972b5dae146c2b854833c2f884420
Artmen_V <viliutich.artem@gmail.com>
2019-09-23 18:37:34 +0700

    try use TS

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./hello.module.css";
 
 export interface HelloProps {
   compiler: string;
@@ -7,8 +8,26 @@ export interface HelloProps {
 
 export const Hello = (props: HelloProps) => {
   return (
-    <h1>
+    <h1 className={styles.main__head}>
       Hello from {props.compiler} and {props.framework} !
     </h1>
   );
 };
+
+
+class Animal {
+  move(distanceInMeters: number = 0) {
+      console.log(`Animal moved ${distanceInMeters}m.`);
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+      console.log('Woof! Woof!');
+  }
+}
+
+const dog = new Dog();
+dog.bark();
+dog.move(10);
+dog.bark();
